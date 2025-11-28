af1998cd9ea4b2e5b38bce14653040c2d6ac4781
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-11-28 15:10:57 +0100

    redefine the migration interface

--- src/models/Migration.ts
+++ src/models/Migration.ts
@@ -3,6 +3,8 @@ import { IResult } from "./Result";
 import { ITool } from "./Tool";
 
 export interface IMigration<T = ITool, H = IResult> {
+    description?: string;
+    tags?: string[];
     commit(tools?: T): Promise<H | void>;
     rollback(tools?: T): Promise<H | void>;
 }
